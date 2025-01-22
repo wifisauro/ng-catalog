9ec22990e966c051b966fb86c04a006b1a859001
Esteban <lopezz.el21.el@gmail.com>
2025-01-21 21:29:18 -0500

    homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DetalleComponent } from './productos/pages/detalle/detalle.component';
+import { ListarComponent } from './productos/pages/listar/listar.component';
+import { ProductosComponent } from './productos/productos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'detalle', component: DetalleComponent},
+    {path: 'listar', component: ListarComponent},
+    {path: 'productos', component: ProductosComponent},
+    {path: '', redirectTo: '/productos', pathMatch: 'full'}
+];

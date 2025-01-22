a721e2d2f8bc117eadb7ab30f14d4b7950f4ed3b
Esteban <lopezz.el21.el@gmail.com>
2025-01-21 19:47:54 -0500

    update

---
+++ src/app/productos/pages/detalle/detalle.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle',
+  imports: [],
+  templateUrl: './detalle.component.html',
+  styleUrl: './detalle.component.css'
+})
+export class DetalleComponent {
+
+}
---
+++ src/app/productos/productos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productos',
+  imports: [],
+  templateUrl: './productos.component.html',
+  styleUrl: './productos.component.css'
+})
+export class ProductosComponent {
+
+}
